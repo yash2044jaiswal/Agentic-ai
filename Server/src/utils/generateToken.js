26cc091ac30f887bde97c8ed468873ce84edaf91
yash2044jaiswal <yash2044jaiswal@gmail.com>
2026-07-12 08:32:22 +0530

    Add fallback for JWT secret and expiration
    Added fallback values for JWT secret and expiration.

--- Server/src/utils/generateToken.js
+++ Server/src/utils/generateToken.js
@@ -4,6 +4,7 @@ import jwt from 'jsonwebtoken';
  * Generates an encrypted JWT access token for authentication.
  * @param {string} userId - User Identification MongoDB ID 
  */
+
 const generateToken = (userId) => {
   const secret = process.env.JWT_SECRET || 'fallback_secret_for_hackathon';
   const expire = process.env.JWT_EXPIRE || '30d';
