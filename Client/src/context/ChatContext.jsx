31d72d97c89d21b2169e5adb0a96ac83bc0842d4
yash2044jaiswal <yash2044jaiswal@gmail.com>
2026-07-12 08:15:53 +0530

    Fix error handling in useChat hook

--- Client/src/context/ChatContext.jsx
+++ Client/src/context/ChatContext.jsx
@@ -8,6 +8,7 @@ export const useChat = () => {
   const context = useContext(ChatContext);
   if (!context) {
     throw new Error('useChat must be used within ChatProvider');
+    
   }
   return context;
 };
